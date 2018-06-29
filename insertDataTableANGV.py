a27dc766e339b7b83fbf6dbcff90d53326cc97e4
Grace Pfohl <pfohl.grace@gmail.com>
2018-06-29 22:13:02 +0000

    first commit

---
+++ insertDataTableANGV.py
@@ -0,0 +1,22 @@
+import sqlite3
+import sys
+conn=sqlite3.connect('sensorsData.db')
+curs=conn.cursor()
+
+# function to insert data on a table
+def add_data (angv, lux):
+    curs.execute("INSERT INTO Door_data values(datetime('now'), (?), (?))", (angv,lux))
+    conn.commit()
+
+# call the function to insert data
+add_data (40, 10)
+add_data (50, 20)
+add_data (60, 50)
+
+# print database content
+print ("\nEntire database contents:\n")
+for row in curs.execute("SELECT * FROM Door_data"):
+    print (row)
+
+# close the database after use
+conn.close()
