684b989e85ebe330236dfdf7c0164e98f722cfc3
Abiodun <iabiodun3@ctisl.gtri.org>
2018-07-09 14:16:53 -0400

    dab on that beautiful api bro

--- server/web_server.py
+++ server/web_server.py
@@ -3,45 +3,43 @@ from flask_restful import Resource, Api
 
 import sqlite3
 
-
-
-'''
-# Retrieve data from database
-def getData():
-	curs = conn.cursor()
-	data = []
-	names = ["door_imu", "door_contact", "box_accel",
-		"box_contact", "room_mic", "room_lux", "room_pir"]
-
-	for name in names:
-		for row in curs.execute("SELECT * FROM " + name + " ORDER BY timestamp ASC LIMIT 1"):
-			timestamp = str(row[0])
-			data.append(row[1])
-
-
-	conn.close()
-	return timestamp, doorIMU, doorContact, boxAccel, boxContact, roomMic, roomLux, roomPIR
-'''
-
-
-def get_latest_data(node, sensor):
-	with sqlite3.connect('sensors-data.db') as conn:
+def get_data_since(node, sensor, time, limit = 500):
+	with sqlite3.connect('sensor-data.db') as conn:
 		cur = conn.cursor()
-		table = node + "_" + sensor
-		for row in cur.execute("SELECT * FROM " + table + " ORDER BY timestamp ASC LIMIT 1"):
-			return str(row[0]), str(row[1])
+		limit = min(500, limit)
+		rows = list(cur.execute( \
+			f"SELECT * FROM sensor_data WHERE " + \
+			f"timestamp > {time} and node = '{node}' and sensor = '{sensor}' " + \
+			f"ORDER BY timestamp DESC LIMIT {limit}"))
+		return rows
 
+def get_data(node, sensor, limit = 500):
+	with sqlite3.connect('sensor-data.db') as conn:
+		cur = conn.cursor()
+		limit = min(500, limit)
+		rows = list(cur.execute( \
+			f"SELECT * FROM sensor_data WHERE " + \
+			f"node = '{node}' and sensor = '{sensor}' " + \
+			f"ORDER BY timestamp DESC LIMIT {limit}"))
+		return rows
 
 class Node(Resource):
-	def get(self, node, sensor):
-		get_latest_data(node, sensor)
+	def get(self, node, sensor, time = None, limit = 1):
+		if time:
+			data = get_data_since(node, sensor, time, limit)
+		else:
+			data = get_data(node, sensor, limit)
+		return list(map(lambda row: { "timestamp": row[0], "value": row[1] }, data))
 
 # main route
 
 def create_app():
 	app = Flask(__name__)
 	api = Api(app)
-	api.add_resource(Node, "/<string:node>/<string:sensor>/")
+	api.add_resource(Node,
+		"/api/<string:node>/<string:sensor>/",
+		"/api/<string:node>/<string:sensor>/limit:<int:limit>",
+		"/api/<string:node>/<string:sensor>/since:<int:time>")
 
 	return app
 
