3df02a0543a515b962d83b911171c62ae2efcf2a
Abiodun <iabiodun3@ctisl.gtri.org>
2018-07-09 13:23:23 -0400

    server works now

--- server/web_server.py
+++ server/web_server.py
@@ -1,60 +1,57 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
 from flask_restful import Resource, Api
 
 import sqlite3
 
-conn = sqlite3.connect('../sensors-data.db')
+
 
 '''
 # Retrieve data from database
 def getData():
-    curs = conn.cursor()
+	curs = conn.cursor()
 	data = []
-	names = ["door_imu", "door_contact", "box_accel", "box_contact", "room_mic", "room_lux", "room_pir"]
+	names = ["door_imu", "door_contact", "box_accel",
+		"box_contact", "room_mic", "room_lux", "room_pir"]
 
 	for name in names:
-    	for row in curs.execute("SELECT * FROM " + name + " ORDER BY timestamp ASC LIMIT 1"):
-        	timestamp = str(row[0])
-        	data.append(row[1])
-        
-	
-    conn.close()
-    return timestamp, doorIMU, doorContact, boxAccel, boxContact, roomMic, roomLux, roomPIR
+		for row in curs.execute("SELECT * FROM " + name + " ORDER BY timestamp ASC LIMIT 1"):
+			timestamp = str(row[0])
+			data.append(row[1])
+
+
+	conn.close()
+	return timestamp, doorIMU, doorContact, boxAccel, boxContact, roomMic, roomLux, roomPIR
 '''
 
+
 def get_latest_data(node, sensor):
-	table = node + "_" * sensor
-	for row in curs.execute("SELECT * FROM " + table + " ORDER BY timestamp ASC LIMIT 1"):
-		return str(row[0]), str(row[1]) 
+	with sqlite3.connect('../sensors-data.db') as conn:
+		cur = conn.cursor()
+		table = node + "_" + sensor
+		for row in cur.execute("SELECT * FROM " + table + " ORDER BY timestamp ASC LIMIT 1"):
+			return str(row[0]), str(row[1])
+
 
 class Node(Resource):
-	def __init__(self, node):
-		super().__init__()
-		self.node = node
-
-	def get(self, sensor):
-		get_latest_data(self.node, sensor)
+	def get(self, node, sensor):
+		get_latest_data(node, sensor)
 
 # main route
 
-@app.route("/")
-def index():
-    return render_template("../client/dist/index.html")
-
-
 def create_app():
-    app = Flask(__name__)
+	app = Flask(__name__)
 	api = Api(app)
-	api.add_resource(Node("door"))
-	api.add_resource(Node("box"))
-	api.add_resource(Node("room"))
+	api.add_resource(Node, "/<string:node>/<string:sensor>/")
 
-    return app
+	return app
 
 app = create_app()
 
+@app.route("/")
+def index():
+	return render_template("../client/dist/index.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
 
 #    host='0.0.0.0', port=80, debug=False
