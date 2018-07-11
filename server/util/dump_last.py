67fa0828b0fdfc00452e770135e4a54d70415d79
Grace <pfohl.grace@gmail.com>
2018-07-11 12:03:20 -0400

    rewrite for new table

--- server/util/dump_last.py
+++ server/util/dump_last.py
@@ -3,5 +3,6 @@ conn=sqlite3.connect('sensorsData.db')
 curs=conn.cursor()
 
 print ("\nLast raw Data logged on database:\n")
-for row in curs.execute("SELECT * FROM incomingData ORDER BY timestamp ASC LIMIT 1"):
-    print (str(row[0])+ " doorIMU ==> "+str(row[1])+" doorContact ==>"+str(row[2])+"boxAccel ==>"+str(row[3])+" boxContact ==>"+str(row[4])+" roomMic ==>"+str(row[5])+" roomLux ==>"+str(row[6])+" roomPIR ==>"+str(row[7]))
+for row in curs.execute("SELECT * FROM sensor_data ORDER BY timestamp ASC LIMIT 1"):
+    print (str(row[0])+ " value : "+str(row[1])+ " node : "+str(row[2])+ " sensor : "+str(row[3]))
+
