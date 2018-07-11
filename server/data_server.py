80280b50ff0567c99b745cd2e13ebee71242f08f
Grace <pfohl.grace@gmail.com>
2018-07-11 10:59:45 -0400

    functions rewritten for new tables

--- server/data_server.py
+++ server/data_server.py
@@ -9,30 +9,46 @@ curs=conn.cursor()
 HOST = '192.168.4.1'
 PORT = 8000
 
-def add_data (doorIMU, doorContact, boxAccel, boxContact, roomMic, roomLux, roomPIR):
-    curs.execute("INSERT INTO incomingData values(timestamp('now'), (?), (?), (?), (?), (?), (?), (?))", (doorIMU, doorContact, boxAccel, boxContact, roomMic, roomLux, roomPIR))
+def add_data_door_imu (door_imu):
+    curs.execute("INSERT INTO door_imu values(timestamp('now'), (?))", (door_imu))
     conn.commit()
 
-def connectionHandler(client, addr):
-    while True:
-        msg = client.recv(1024).decode()
-        if not msg:
-            break
-        print(msg)
-        add_data(int(msg), 10, 10, 10, 10, 10, 10)
-        client.send(msg.encode())
-    client.close()
+def add_data_door_contact (door_contact):
+    curs.execute("INSERT INTO door_contact values(timestamp('now'), (?))", (door_contact))
+    conn.commit()
+
+def add_data_box_accel (box_accel):
+    curs.execute("INSERT INTO box_accel values(timestamp('now'), (?))", (box_accel))
+    conn.commit()
+
+def add_data_room_lux (room_lux):
+    curs.execute("INSERT INTO room_lux values(timestamp('now'), (?))", (room_lux))
+    conn.commit()
+
+def add_data_room_pir (room_pir):
+    curs.execute("INSERT INTO room_pir values(timestamp('now'), (?))", (room_pir))
+    conn.commit()
+
+# def connectionHandler(client, addr):
+#     while True:
+#         msg = client.recv(1024).decode()
+#         if not msg:
+#             break
+#         print(msg)
+#         add_data_door_imu(int(msg))
+#         client.send(msg.encode())
+#     client.close()
 
 
-s = socket.socket()
+# s = socket.socket()
 
-print("Starting server...")
-print("Waiting for clients")
+# print("Starting server...")
+# print("Waiting for clients")
 
-s.bind((HOST, PORT))
-s.listen(1)
+# s.bind((HOST, PORT))
+# s.listen(1)
 
-while True:
-    c, addr = s.accept()
-    connectionHandler(c, addr)
-s.close()
+# while True:
+#     c, addr = s.accept()
+#     connectionHandler(c, addr)
+# s.close()
