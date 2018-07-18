4ce5f1fe507aa15afd77ae04042867010bd89b8d
Abiodun <iabiodun3@ctisl.gtri.org>
2018-07-18 13:15:56 -0400

    recreated api server in node js

---
+++ server/web/api/sensor.ts
@@ -0,0 +1,48 @@
+import * as Koa from "koa";
+import * as KoaRouter from "koa-router";
+import * as fs from "fs-extra";
+import * as path from "path";
+
+export default (basePath: string) => {
+    async function handler(ctx: Koa.Context) {
+        const POINTER_SIZE = 4;
+        const RECORD_SIZE = 9;
+        const RECORD_COUNT = 2400;
+        
+        const limit = Math.min(RECORD_COUNT, ctx.params.limit || 500);
+        const { since, node, sensor } = ctx.params;
+
+        const fn = path.join(basePath, "cache", node, sensor);
+        if (!await fs.pathExists(fn)) {
+            ctx.response.status = 404;
+            return;
+        }
+
+        const handle = await fs.open(fn, "r");
+        const buf = Buffer.alloc(RECORD_SIZE);
+        await fs.read(handle, buf, 0, POINTER_SIZE, 0);
+        const position = buf.readInt32LE(0);
+        const start = (position - limit + RECORD_COUNT) % RECORD_COUNT;
+        const records = [];
+        
+        for(let current = start; current != position; current = (current + 1) % RECORD_SIZE) {
+            await fs.read(handle, buf, 0, RECORD_SIZE, POINTER_SIZE + current * RECORD_SIZE);
+            const timestamp = buf.readDoubleLE(0);
+            const value = buf[8];
+
+            if(timestamp === 0) continue;
+
+            records.unshift({ timestamp, value });
+        }
+
+        await fs.close(handle);
+
+        ctx.response.body = records;
+    }
+
+    return new KoaRouter()
+        .get("/:node([A-Za-z0-9]+)/:sensor([A-Za-z0-9]+)", handler)
+        .get("/:node([A-Za-z0-9]+)/:sensor([A-Za-z0-9]+)/limit::limit(\\d+)", handler)
+        .get("/:node([A-Za-z0-9]+)/:sensor([A-Za-z0-9]+)/since::since(\\d+)", handler)
+        .get("/:node([A-Za-z0-9]+)/:sensor([A-Za-z0-9]+)/since::since(\\d+)/limit::limit(\\d+)", handler);
+};
---
+++ server/web/web_server.ts
@@ -0,0 +1,61 @@
+import * as path from "path";
+import * as http from "http";
+
+import * as Koa from "koa";
+import * as KoaRouter from "koa-router";
+
+import * as winston from "winston";
+
+import ThreatApi from "./api/threat";
+import SensorApi from "./api/sensor";
+
+import * as sqlite from "sqlite3";
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.splat(),
+                winston.format.printf(info => {
+                    return `${info.timestamp} | ${info.level}: ${info.message}`;
+                }),
+                winston.format.colorize()),
+            level: "debug",
+            handleExceptions: true
+        })
+    ],
+    levels: winston.config.syslog.levels
+});
+
+const basePath = path.join(__dirname, "..");
+const dbPath = path.join(basePath, "data.db");
+
+logger.info(`Base path: ${basePath}`);
+logger.info(`Database path: ${dbPath}`);
+
+const db = new sqlite.Database(dbPath);
+
+const app = new Koa();
+const router = new KoaRouter();
+const api = new KoaRouter();
+
+api.use("/threat", ThreatApi(db).routes());
+api.use("/sensor", SensorApi(basePath).routes());
+
+router.use("/api", api.routes());
+
+app.use(async (ctx, next) => {
+    logger.log("debug", `request: ${ctx.protocol} ${ctx.method} ${ctx.href}`);
+    await next();
+});
+
+app.use(router.routes());
+
+
+const serverFactory = http.createServer;
+const server = serverFactory(app.callback());
+const port = process.env.PORT || 8000;
+server.listen(port);
+
+logger.info(`Server is running on port ${port}`);
