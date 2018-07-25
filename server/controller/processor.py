9a1879e993f5585d19ca1bfedc20b0a762c7f63b
Ibiyemi Abiodun <ibiyemi@intulon.com>
2018-07-25 12:27:25 -0400

    server now has locks to prevent db errors

--- server/controller/processor.py
+++ server/controller/processor.py
@@ -4,6 +4,8 @@ from rx.core import Disposable
 import sqlite3
 import time
 
+import threading
+
 import numpy as np
 from monitor import sigmoid
 
@@ -17,6 +19,7 @@ class ThreatProcessor:
         self.sensitivity = sensitivity
         self.query = Observable.combine_latest(threats, lambda *data: data)
         self.subscription: Disposable = self.query.subscribe(self.on_threat)
+        self.db_lock = threading.Lock()
 
     def on_threat(self, buffer: [int]):
         if -1 in buffer:
@@ -26,7 +29,8 @@ class ThreatProcessor:
         std = np.std(buffer)
         b_max = np.max(buffer)
 
-        threat_score = (np.sum(buffer) + b_max) / (sigmoid(std / (b_max + 1)) * std + 1)
+        threat_score = (np.sum(buffer) + b_max) / \
+            (sigmoid(std / (b_max + 1)) * std + 1)
         fac = sigmoid((threat_score - self.prev_score) * 2.7)
         threat_score = threat_score * fac + self.prev_score * (1 - fac)
 
@@ -35,8 +39,11 @@ class ThreatProcessor:
         t = round(time.time(), 3)
         ps = round(self.prev_score, 1)
         ts = round(threat_score, 1)
+
         self.conn.execute(
             f"INSERT INTO threats VALUES ({t}, NULL, NULL, {ps}, {ts})")
-        self.conn.commit()
+        if self.db_lock.acquire(blocking=False):
+            self.conn.commit()
+            self.db_lock.release()
 
         self.prev_score = threat_score
